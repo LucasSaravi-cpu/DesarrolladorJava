7b7978f8f2c1074022e4b3bcd09a31d62a0a22f4
LucasSaravi-cpu <126580800+LucasSaravi-cpu@users.noreply.github.com>
2023-03-03 14:40:22 -0300

    Update Clase1Ejercicio1c.java

--- Clase1Ejercicio1c.java
+++ Clase1Ejercicio1c.java
@@ -1,25 +1,50 @@
 
+import java.util.Scanner;
+
 public class Clase1Ejercicio1c {
 	
 	public static void main(String[] args) {
 		
-	    /* for(int  numeroInicio=14 ; numeroInicio>=5 ;numeroInicio--) {  	
-			if ((numeroInicio % 2) == 0) {
-				
-			System.out.println(numeroInicio);	*/
 		
+		Scanner entrada=new Scanner(System.in);
+		System.out.println("Ingrese si quiere organizar los numeros de forma par o impar ");
+		String palabra=entrada.nextLine().toLowerCase();
+		
+
+
+		switch (palabra) {
+		
+		case "par":
+			
+			for(int  numeroInicio=14 ; numeroInicio>=5 ;numeroInicio--) {  	
+				if ((numeroInicio % 2) == 0) {
+					
+				System.out.println(numeroInicio);
+				}
+			}
+			break;
+			
+		case "impar":
+			
 			for(int  numeroInicio=14 ; numeroInicio>=5 ;numeroInicio--) {  
 				if ((numeroInicio % 2) != 0) {
 		
 		    System.out.println( numeroInicio);		
 			
-			
+				}
 			}
+			break;
+			
+		default:
+			System.out.println("La palabra ingresada es incorrecta");
 		}
+		
+	    
+			}
+					
+			
 	}
 
-
-}
 
 
 		
