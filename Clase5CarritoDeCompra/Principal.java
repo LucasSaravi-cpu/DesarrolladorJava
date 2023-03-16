b3afe4d203aa5db79851bafcb51e4a9b600b5fc3
LucasSaravi-cpu <126580800+LucasSaravi-cpu@users.noreply.github.com>
2023-03-16 10:33:28 -0300

    Update Principal.java

--- Clase5CarritoDeCompra/Principal.java
+++ Clase5CarritoDeCompra/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
 	    public static void main(String[] args) {
 	        // Creamos algunos productos
 	        Producto producto1 = new Producto("Camisa", 29.99, 2);
-	        Producto producto2 = new Producto("Pantalón", 39.99, 1);
+	        Producto producto2 = new Producto("PantalÃ³n", 39.99, 1);
 	        Producto producto3 = new Producto("Zapatos", 59.99, 1);
 
 	        // Creamos un carrito de compras
@@ -34,7 +34,7 @@ public class Principal {
 	        }
 	        if (descuentoPorCantidad.aplicaDescuento(cantidadTotal)) {
 	            double precioConDescuento = descuentoPorCantidad.aplicarDescuento(carrito.getPrecioTotal());
-	            System.out.println("Descuento aplicado: " + descuentoPorCantidad.getNombre() + " (cantidad mínima: " + descuentoPorCantidad.getCantidadMinima() + ")");
+	            System.out.println("Descuento aplicado: " + descuentoPorCantidad.getNombre() + " (cantidad mÃ­nima: " + descuentoPorCantidad.getCantidadMinima() + ")");
 	            System.out.println("Precio con descuento: $" + precioConDescuento);
 	        }
 
@@ -44,8 +44,53 @@ public class Principal {
 	        // Verificamos si aplica el descuento por monto
 	        if (descuentoPorMonto.aplicaDescuento(carrito.getPrecioTotal())) {
 	            double precioConDescuento = descuentoPorMonto.aplicarDescuento(carrito.getPrecioTotal());
-	            System.out.println("Descuento aplicado: " + descuentoPorMonto.getNombre() + " (monto mínimo: $" + descuentoPorMonto.getMontoMinimo() + ")");
+	            System.out.println("Descuento aplicado: " + descuentoPorMonto.getNombre() + " (monto mÃ­nimo: $" + descuentoPorMonto.getMontoMinimo() + ")");
 	            System.out.println("Precio con descuento: $" + precioConDescuento);
 	        }
 	    }
 	}
+
+/*
+
+//Ejercicio 5b carga los datos por un txt y devuelve el precio total de esos productos cargados
+Carrito carrito = new Carrito();
+	    	try {
+	            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Principal\\Desktop\\Curso Desarrollador de java\\Clase 5\\precios.txt"));
+	            
+	            
+	               
+	         
+	            String line = reader.readLine();
+	            
+	           
+	            while ((line = reader.readLine()) != null) {
+	            	
+  	           String[] parts = line.split(",");
+	            
+	           
+	   	        int cant = Integer.parseInt(parts[0]);
+	   	        double precio = Double.parseDouble(parts[1]);
+	   	        String produ = parts[2];
+	  
+	   	        //   Creamos algunos productos que se van creando por linea a medida que va pasando el while
+	   	      Producto producto = new Producto(produ, precio, cant);
+	      			            
+	   	   
+
+	        // Agregamos los productos al carrito que se van creando a medida que pasa el while
+	        carrito.agregarItem(producto, cant);
+	   	      
+	   	 		  
+	            }
+			 
+	            System.out.println("Precio total: $" + carrito.getPrecioTotal());
+	           
+	            reader.close();
+	           
+	        } catch (IOException e) {
+	            System.out.println("Error al leer el archivo");
+	        }
+
+
+
+*/
